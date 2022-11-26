de7df7d615bb4733736daeceab756eb02658a47b
krittin <aomgg555@gmail.com>
2022-11-26 11:24:59 +0700

    26/11/2022

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  // res.send('respond with a resource');
+
+  res.status(200).json({
+    Fullname : 'Krittin kamkar'
+  })
+});
+
+router.get('/bio', function(req, res, next) {
+  
+  res.status(200).json({
+    fullname: 'Krittin kamkar',
+    nickname: 'Aom',
+    hobby:'Sleep',
+    gitusername: 'Krittin33333'
+  })
+  
 });
 
 module.exports = router;
