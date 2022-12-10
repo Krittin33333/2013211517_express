b366b7a755468b5cbfbf9158ea60ce2d0d49bfb2
krittin <aomgg555@gmail.com>
2022-12-10 15:25:12 +0700

    10/12/2022 CUND with mongoose

--- controllers/staffController.js
+++ controllers/staffController.js
@@ -3,12 +3,40 @@ const Staff = require('../models/staff')
 exports.index = async(req, res, next) => {
       // res.send('respond with a resource');
 
-      const staff = await Staff.find()
+      const staff = await Staff.find().sort({_id: -1})
 
       res.status(200).json({
         data: staff
       })
     };
+
+exports.show = async(req, res, next) => {
+        
+    // by id
+    try{
+        const { id } = req.params
+  
+        const staff = await Staff.findOne({
+            _id : id
+        })
+  
+        if(!staff){
+            throw new Error('ไม่พบผู้ใช้งาน')
+        }
+        else{
+            res.status(200).json({
+                data: staff
+              })
+        }
+
+        
+    } catch (error){
+        res.status(400).json({
+            error: { Message:"เกิดข้อผิดพลาด : " + error.message}
+          })
+    }
+        
+      };
 
 exports.insert = async (req, res, next) => {
         // res.send('respond with a resource');
@@ -25,3 +53,68 @@ exports.insert = async (req, res, next) => {
           Message: "เพิ่มข้อมูลเรียบร้อยแล้ว",
         })
       };
+
+exports.destroy = async(req, res, next) => {
+        
+        // by id
+        try{
+            const { id } = req.params
+      
+            const staff = await Staff.deleteOne({
+                _id : id
+            })
+            
+            if(staff.deletedCount === 0){
+                throw new Error('ไม่สามารถลบข้อมูลได้ / ไม่พบผู้ใช้งาน')
+            }
+            else{
+                res.status(200).json({
+                    message: "ลบข้อมูลเรียบร้อยแล้ว",
+                  })
+            }
+            
+        } catch (error){
+            res.status(400).json({
+                error: { Message:"เกิดข้อผิดพลาด : " + error.message}
+              })
+        }
+            
+          };
+
+exports.update = async (req, res, next) => {
+            
+
+        try{
+            const { id } = req.params
+            const { name,salary } = req.body
+
+            // 1
+            // const staff = await Staff.findById(id)
+            // staff.name = name
+            // staff.salary = salary
+            // await staff.save()
+
+            // 2
+            // const staff = await Staff.findByIdAndUpdate(id,{
+            //     name : name,
+            //     salary : salary
+            // })
+
+             const staff = await Staff.updateOne({ _id : id},{
+                name : name,
+                salary : salary
+            })
+
+            console.log(staff)
+
+
+            res.status(200).json({
+              Message: "แก้ไขข้อมูลเรียบร้อยแล้ว",
+            })
+
+        } catch (error){
+
+        }
+    
+         
+          };
--- routes/staff.js
+++ routes/staff.js
@@ -5,6 +5,12 @@ const staffController = require('../controllers/staffController')
 /* GET users listing. */
 router.get('/', staffController.index);
 
+router.get('/:id', staffController.show);
+
 router.post('/', staffController.insert);
+
+router.delete('/:id', staffController.destroy);
+
+router.put('/:id', staffController.update);
 
 module.exports = router;
