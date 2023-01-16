ff4fa6d934408c78bac661a9b590270000d808a5
krittin <aomgg555@gmail.com>
2023-01-16 12:28:17 +0700

    TNI

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,9 +1,3 @@
 module.exports = (err, req, res, next) => {
    
     const statusCode = err.statusCode || 500
-
-    res.status(statusCode).json({
-        status_code :statusCode,
-        message: err.message
-      })
-}
