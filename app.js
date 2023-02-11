7dcef9e2c576076b158f5cb355fbc9877a5f288c
Krittin <aomgg555@gmail.com>
2023-02-11 09:44:37 +0700

    TEST

--- app.js
+++ app.js
@@ -25,8 +25,8 @@ app.use(express.json({
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(passport.initialize());
 
+app.use(passport.initialize()); //passportJWT
 
 app.use('/', indexRouter);
 app.use('/user', usersRouter);
